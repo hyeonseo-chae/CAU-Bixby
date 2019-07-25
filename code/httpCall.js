574e00c978b0e3b5645fbba14a540cf0e7bb66b6
hyeonseo <eric71804122@gmail.com>
2019-07-25 15:37:09 +0900

    view 수정중

--- code/httpCall.js
+++ code/httpCall.js
@@ -1,44 +1,6 @@
-module.exports.function = function httpCall () {
-  
+module.exports.function = function httpCall () { //공지사항
   var http = require('http')
-  URL = 'https://mportal.cau.ac.kr/portlet/p005/p005.ajax'
-  var headers = {  
-    format : "json",  
-    headers : {  
-      "Content-Type":'application/json'  
-    }} 
-var params = { "tabs":"1","tabs2":"10","daily":0 };   
-var result = http.postUrl(URL, params, headers);  
-return result
-  
-  /*for(var i = 0; i < booksInfo.length; i++){
-    let url = null;
-    let description = null;
-    
-    if(booksInfo[i].volumeInfo.imageLinks == undefined){
-      url = "/icon/bixby.png";
-    }else{
-      url = booksInfo[i].volumeInfo.imageLinks.thumbnail;
-    }
-    
-    if(booksInfo[i].volumeInfo.description != undefined){
-      description = booksInfo[i].volumeInfo.description.replace(/(<([^>]+)>)/ig,"");
-    }
-    
-    result.push(
-      {
-        kind: booksInfo[i].kind,
-        id: booksInfo[i].id,
-        detailLink: booksInfo[i].selfLink,
-        volume:{
-          title: booksInfo[i].volumeInfo.title,
-          authors: booksInfo[i].volumeInfo.authors,
-          description: description,
-          imageLinks: url
-        }
-      }
-    );
-  }*/
-}
- 
+  var result = http.getUrl("https://www.cau.ac.kr/ajax/FR_SVC/BBSViewList2.do?pageNo=1&pagePerCnt=20&MENU_ID=100&SITE_NO=2&BOARD_SEQ=4&BOARD_TYPE=C0301",{format : 'json'})
 
+  return result
+} 
