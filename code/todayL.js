ea944f00dbcd0535d30c4159096e82ad4b1a8fbd
hyeonseo <eric71804122@gmail.com>
2019-07-26 18:56:19 +0900

    view 처리 완료 및 학식 완료

---
+++ code/todayL.js
@@ -0,0 +1,21 @@
+module.exports.function = function todayL () {
+  
+  var http = require('http')
+  URL = 'https://mportal.cau.ac.kr/portlet/p005/p005.ajax'
+  var headers = {  
+    format : "json",  
+    headers : {  
+      "Content-Type":'application/json'  
+    }} 
+var params = { "tabs":"1","tabs2":"20","daily":0 };   
+var result = http.postUrl(URL, params, headers);  
+return result
+  /*tabs2 = 10(조식)
+20(중식)
+40(석식)
+daily = 0(오늘)
+1(내일)*/
+  
+}
+ 
+
