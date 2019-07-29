b2ed7494523b710cbab8b32377c304486f33aed2
hyeonseo <eric71804122@gmail.com>
2019-07-30 04:33:09 +0900

    포탈 공지사항 추가

---
+++ code/potalnotice.js
@@ -0,0 +1,21 @@
+module.exports.function = function potalnotice () {
+  
+  var http = require('http')
+  URL = 'https://mportal.cau.ac.kr/system/admin/board/B0004/selectPostList.ajax'
+  var headers = {  
+    format : "json",  
+    headers : {  
+      "Content-Type":'application/json'  
+    }} 
+var params = {"bbsId": "B0004", "bbsType": "1", "categoryCd": "", "searchType": "title", "searchWord": ""};   
+var result = http.postUrl(URL, params, headers);  
+return result
+  /*tabs2 = 10(조식)
+20(중식)
+40(석식)
+daily = 0(오늘)
+1(내일)*/
+  
+}
+ 
+
